ec0b4b7892f4644b4b1648044c5a7e5b00d4d322
Pedro Paulo <pedropscontatoprof@gmail.com>
2025-07-21 14:45:48 -0300

    Solving middle of the linked list , and reverse linked list questions

---
+++ linked-lists/reverse-linked-list.py
@@ -0,0 +1,15 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        new_list = None
+
+        while head:
+            next_node = head.next
+            head.next = new_list
+            new_list = head
+            head = next_node
+        return new_list
